756c4cc8465ca59564ba231a73e33afaf7aca287
Damien Vallet <damienjdvallet@gmail.com>
2023-12-22 17:12:05 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,2 +1,14 @@
 // create web server
-//
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+app.use(bodyParser.urlencoded({extended: false}));
+// set template engine
+app.locals.pretty = true;
+app.set('views', './views_file');
+app.set('view engine', 'jade');
+// create web server
+app.listen(3000, function(){
+  console.log('Connected, 3000 port!');
+});
